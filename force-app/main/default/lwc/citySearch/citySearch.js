be85da78900919df660cb5b42f6896be7778696d
dikla <dikla.giratt@gmail.com>
2022-10-02 22:22:14 +0300

    update html use LDS style

--- force-app/main/default/lwc/citySearch/citySearch.js
+++ force-app/main/default/lwc/citySearch/citySearch.js
@@ -2,6 +2,28 @@ import { LightningElement } from 'lwc';
 
 export default class CitySearch extends LightningElement {
     city;
+    value = this.city;
+
+    get cityOptions() {
+        return [
+            { label: 'New York', value: 'New York' },
+            { label: 'Tokyo', value: 'Tokyo' },
+            { label: 'Delhi', value: 'Delhi' },
+            { label: 'São Paulo', value: 'São Paulo' },
+            { label: 'Los Angeles', value: 'Los Angeles' },
+            { label: 'Paris', value: 'Paris' },
+            { label: 'Tel Aviv', value: 'Tel Aviv' },
+            { label: 'Miami', value: 'Miami' },
+            { label: 'Washington', value: 'Washington' },
+            { label: 'Berlin', value: 'Berlin' },
+            { label: 'Madrid', value: 'Madrid' },
+            { label: 'Rome', value: 'Rome' },
+        ];
+    }
+    handleChangeCityList(event) {
+        this.value = event.detail.value;
+        this.city = this.value;
+    }
 
     handleChangeCity(event) {
         this.city = event.target.city;
--- force-app/main/default/lwc/weatherApi/weatherApi.js
+++ force-app/main/default/lwc/weatherApi/weatherApi.js
@@ -1,15 +1,18 @@
 import { LightningElement, api } from 'lwc';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
-const API_KEY = ''
+const API_KEY = '';
 export default class WeatherApi extends LightningElement {
     @api city
     weather
     temp
     imgUrl
+    description
+    isLoading = true;
 
     renderedCallback() {
         this.fetchWeather();
     }
+
     fetchWeather() {
         fetch(BASE_URL + this.city + '&appid=' + API_KEY + '&units=metric')
             .then(response => response.json())
@@ -17,6 +20,8 @@ export default class WeatherApi extends LightningElement {
                 this.temp = response.main.temp;
                 this.weather = response.weather[0];
                 this.imgUrl = 'http://openweathermap.org/img/wn/' + this.weather.icon + '@2x.png';
+                this.description = this.weather.description;
+                this.isLoading = false;
             })
             .catch(error => console.error(error))
     }
